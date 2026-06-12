df936d3c6a7fe0cefe7c4719e7a8b3d016121c4c
ssajjad4 <ssajjad4@gmail.com>
2026-06-12 00:08:58 -0400

    Change response method to include status code

--- api/price.js
+++ api/price.js
@@ -8,5 +8,5 @@ export default async function handler(req, res) {
   const data = await response.json();
 
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.json(data);
+  res.status(200).json(data);
 }
